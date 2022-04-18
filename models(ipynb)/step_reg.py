2a33baf525ed1d4717ea026db259f9c5ff03b4fb
royull <87632610+royull@users.noreply.github.com>
2022-04-18 16:23:04 -0400

    add forward selection

--- models(ipynb)/step_reg.py
+++ models(ipynb)/step_reg.py
@@ -1,47 +1,30 @@
 import pandas as pd
 import statsmodels.api as sm
 
-def forward_regression(X, y,
-                       threshold_in,
-                       verbose=False):
+def forward_regression(X, y):
+    '''
+    Input
+    X,y: training matrix (without intercept)
+    Return
+    model: ols model fitted with intercept on the selected features
+    feature_selected: selected features
+    '''
     initial_list = []
     included = list(initial_list)
-    while True:
-        changed=False
+    feature_num = len(X.columns)
+    best_aics = pd.Series(index={i for i in range(feature_num)})
+    best_features = list(it.repeat([],feature_num))
+    for k in range(feature_num):
         excluded = list(set(X.columns)-set(included))
-        new_pval = pd.Series(index=excluded)
+        new_aic = pd.Series(index=excluded)
         for new_column in excluded:
             model = sm.OLS(y, sm.add_constant(pd.DataFrame(X[included+[new_column]]))).fit()
-            new_pval[new_column] = model.pvalues[new_column]
-        best_pval = new_pval.min()
-        if best_pval < threshold_in:
-            best_feature = new_pval.idxmin()
-            included.append(best_feature)
-            changed=True
-            if verbose:
-                print('Add  {:30} with p-value {:.6}'.format(best_feature, best_pval))
-
-        if not changed:
-            break
-
-    return included
-
-def backward_regression(X, y,
-                           threshold_out,
-                           verbose=False):
-    included=list(X.columns)
-    while True:
-        changed=False
-        model = sm.OLS(y, sm.add_constant(pd.DataFrame(X[included]))).fit()
-        # use all coefs except intercept
-        pvalues = model.pvalues.iloc[1:]
-        worst_pval = pvalues.max() # null if pvalues is empty
-        if worst_pval > threshold_out:
-            changed=True
-            worst_feature = pvalues.idxmax()
-            included.remove(worst_feature)
-            if verbose:
-                print('Drop {:30} with p-value {:.6}'.format(worst_feature, worst_pval))
-        if not changed:
-            break
-    return included
+            new_aic[new_column] = model.aic
+        best_aic = new_aic.min()
+        best_aics[k] = best_aic
+        best_feature = new_aic.idxmin()
+        included.append(best_feature)
+        best_features[k] = included.copy()
+    feature_selected = best_features[best_aics.idxmin()]
+    model = sm.OLS(y, sm.add_constant(pd.DataFrame(X[feature_selected]))).fit()
+    return model,feature_selected
